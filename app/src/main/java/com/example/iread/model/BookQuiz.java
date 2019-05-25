65d15b2cf596d9b99c0199fe77068f2f45c00906
AMINE-ADIB <pfeapp12@gmail.com>
2019-05-25 03:41:14 +0100

    ok

--- app/src/main/java/com/example/iread/model/BookQuiz.java
+++ app/src/main/java/com/example/iread/model/BookQuiz.java
@@ -6,12 +6,14 @@ public class BookQuiz {
     public String image;
     public String name;
     public String star;
+    //public String classement;
 
     public BookQuiz(){}
     public BookQuiz(String auteur,String image,String name,String star){
         this.image=image;
         this.name=name;
         this.star=star;
+        //this.classement=classement;
     }
     public String getImage(){return image;}
     public String getName(){return name;}
@@ -21,6 +23,9 @@ public class BookQuiz {
     public void setStar(String star){this.star =star;}
     public String getAuteur(){return auteur;}
     public void setAuteur(String auteur){this.auteur =auteur;}
+    //public String getClassement(){return classement;}
+    //public void setClassement(String classement){this.classement =classement;}
+
 
 
 
